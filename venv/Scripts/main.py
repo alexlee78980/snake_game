ec943cf87e5a35293378e6398571adbfa979b80b
alexlee78980 <78987809+alexlee78980@users.noreply.github.com>
2021-12-26 22:02:06 -0800

    Initial commit

---
+++ venv/Scripts/main.py
@@ -0,0 +1,39 @@
+from turtle import Screen, Turtle
+import time
+import snake
+import food
+import scoreboard
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = snake.Snake()
+food = food.Food()
+score = scoreboard.Scoreboard()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food)<15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor()< -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280 or snake.crash():
+        game_is_on = False
+        score.game_over()
+
+
+
+
+
+screen.exitonclick()
